229360b7c8a69aafc43df32d293edf814f24f1a4
hayawbst <haya@websultanate.com>
2023-05-17 13:04:02 +0530

    github deploy

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Components/Home";
+
+function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const handleThemeChange = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
+  useEffect(() => {
+    const storedTheme = localStorage.getItem("theme");
+    if (storedTheme) {
+      setIsDarkMode(storedTheme === "dark");
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("theme", isDarkMode ? "dark" : "light");
+  }, [isDarkMode]);
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            index
+            exact
+            path="/home"
+            element={
+              <Home
+                isDarkMode={isDarkMode}
+                handleThemeChange={handleThemeChange}
+              />
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+      {/* <div className={isDarkMode ? "App-dark" : "App-light"}>
+        <h1>Light and Dark Theme Example</h1>
+        <button onClick={handleThemeChange}>
+          {isDarkMode ? "Switch to Light Mode" : "Switch to Dark Mode"}
+        </button>
+        <p>
+          This is some sample text that demonstrates the use of light and dark
+          themes in a React app.
+        </p>
+      </div> */}
+    </>
+  );
+}
+
+export default App;
